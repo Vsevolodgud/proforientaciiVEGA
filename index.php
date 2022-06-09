0c52970b3fb52d61a054ecd902de7ece783f5043
Vsevolodgud <71864845+Vsevolodgud@users.noreply.github.com>
2022-06-09 17:17:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+  <meta http-equiv="X-RU-Compatible" co\charset="ie=egde">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <title> test</title>
+  <head>
+    <body>
+      <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="me-2" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>
+        <span class="fs-4">Test</span>
+      </a>
+      
+    </div>
+    <div clss="container mt-5">
+
+       <div class="row">
+<div class="col-md-6">
+ 	<p class="test-question" id="question-1"><strong>Вопрос№1. выберете предметы которые лучше всего вам довались.</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-1"> Техгические (Математика, информатика, физика)</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-1"> Гуманитарные (Русский, литература)</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-1"> Другие(физ-ра, ОБЖ,)</div>
+ 	</div>
+
+<div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№2. Ты можешь долго сидеть за компьютером и это вас не утомляет.</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-2"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-2"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-2"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№3. Какой тип работы вас привлекает больше? </strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-3"> Создавать и придумывать что то новое</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-3"> Работать руками и ногами выполняя чужие указания</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-3"> затрудняюсь ответить</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№4. Легко ли вам довалась математика в школе?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-4"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-4"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-4"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№5. На технической выставке меня больше привлечет </strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-13"> их практическое применение</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-14"> внешний вид экспанатов</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-15"> их внутренее устройство</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№6. Будущее людей определяет?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-16"> Развитие производства</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-17"> научные открытия </div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-18"> Нетвзаимопонимание между людьми</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№7. Работая я хотел бы больше времени?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-19"> заниматься расчетами вести документы</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-20"> общаться с самыми разными людьми</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№8. Работа связанная с компьютерами учетами и документами скучная?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-21"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-22"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-23"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№9. Раздражает беспорядок?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-24"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-25"> Не могу точно сказать</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-26"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№10. Привлекает ли вас IT-сфера?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-5"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-5"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-5"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№11. Мне бы хотелось хорошо зарабатывать и иметь возможность путешевствовать?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-30"> Конечно</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-31"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-32"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№12. Я довольно хорошо структурирую и понимаю задачи которые мне приходиться делать?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-33"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-34"> Не могу ответить точно</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-35"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№13. Ты в детстве увлеченно играл в конструктор?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-36"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-37"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-38"> Нет</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№14. Что самое общее между бабочкой и слоном?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-39"> они оба животные</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-40"> у них есть хобот</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-41"> уши слона как крылья бабочки</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№15. Выбери лишнее. </strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-42"> Системный Администратор</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-43"> Тестировщик</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-44"> програмист</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№16. Представьте: вы купили разобраный велосипед и решили собрать его самостоятельно .Ваши действия:</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-45"> буду четко действовать инструкции</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-46"> Соберу без всяких инструкций: вдруг смогу лучше?</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-47"> Вспомню как он выглядел в магазине и попробую собрать сам, чего там сложного.</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№17. Как вы думаете, какая часть рабочего процесса программистов обычно не радует?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-48"> поиск решения </div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-49"> Разбор чужого кода.</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-50"> Игра в настольный футбол.</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№18.На сколько вы хорошо знаете английский язык по 10 бальной системе?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-51"> 10 баллов.</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-52"> 5 баллов.</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-53"> 2 балла</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№19. Что из ниже перечисленного не является языком программирования</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-54"> PHP</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-55"> HTML</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-56"> C#</div>
+      <div class="test-answer"><input type="checkbox" value="0" name="answer-57"> JavaScript</div>
+ 	</div>
+   <div class="col-md-6">
+ 	<p class="test-question" id="question-2"><strong>Вопрос№20. Сложно ли тебе работать в команде где вссе выполняют свой участок работы?</strong></p>
+ 	 		<div class="test-answer"><input type="checkbox" value="2" name="answer-58"> Да</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="1" name="answer-59"> Затрудняюсь ответить</div>
+ 	 		<div class="test-answer"><input type="checkbox" value="0" name="answer-60"> Нет</div>
+ 	</div>
+</div>
+
+    </div>
+   <form action="result.php" method="POST">
+    <button type="submit">Результат</button>
+    </from>
+
+    </body>
+  </head>
+
+
+
+
